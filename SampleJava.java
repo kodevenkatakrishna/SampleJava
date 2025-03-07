1a64b73ebd4ca088a473756679550a200decbf6a
venkatakrishna.kode <venkatakrishna.kode@hcl.com>
2025-03-07 23:51:47 +0530

    First Commit

---
+++ SampleJava.java
@@ -0,0 +1,10 @@
+import com.sun.jdi.Value;
+
+public class SampleJava {
+    public static void main(String[] args){
+        int[] num = {1,2,3,4,5,6,7,8,9};
+        for(int Value:num){
+            System.out.println(Value);
+        }
+    }
+}
