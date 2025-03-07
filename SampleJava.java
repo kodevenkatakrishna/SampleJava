a2840bd5e73377796fb408e2b11698f4af590caf
venkatakrishna.kode <venkatakrishna.kode@hcl.com>
2025-03-07 23:59:58 +0530

    Third Commit

--- SampleJava.java
+++ SampleJava.java
@@ -4,7 +4,7 @@ public class SampleJava {
     public static void main(String[] args){
         int[] num = {1,2,3,4,5,6,7,8,9};
         for(int Value:num){
-            System.out.println(Value);
+            System.out.println(Value+Value);
         }
     }
 }
